166d6cd1df72cfbc66443e3d2a1747f2fba211b5
HarrySmith81224 <107133689+HarrySmith81224@users.noreply.github.com>
2023-12-09 23:26:23 +0000

    commit5

--- elements/element01/src/createStartScene.ts
+++ elements/element01/src/createStartScene.ts
@@ -1,7 +1,3 @@
-//-----------------------------------------------------
-//TOP OF CODE - IMPORTING BABYLONJS
-//import "@babylonjs/core/Debug/debugLayer";
-//import "@babylonjs/inspector";
 import {
     Scene,
     ArcRotateCamera,
@@ -22,27 +18,23 @@ import {
     PointLight,
     DirectionalLight,
   } from "@babylonjs/core";
-  //----------------------------------------------------
-  
-  //----------------------------------------------------
-  //MIDDLE OF CODE - FUNCTIONS
+
+
   function createBox(scene: Scene, px: number, py: number, pz: number, sx: number, sy: number, sz: number, rotation: boolean) {
     let box = MeshBuilder.CreateBox("box",{size: 1}, scene);
     box.position = new Vector3(px, py, pz);
     box.scaling = new Vector3(sx, sy, sz);
-    //Old position without the parameters
-    //This will add every 'createBox' generated in the same position
-    //box.position.y = 3;
+    box.receiveShadows = true;
+
 
     if (rotation) {
       scene.registerAfterRender(function () {
-        box.rotate(new Vector3(4, 8, 2)/*axis*/, 0.02/*angle*/, Space.LOCAL);
+        box.rotate(new Vector3(10, 7, 5)/*axis*/, 0.4/*angle*/, Space.LOCAL);
       });
     }
     return box;
   }
 
-  //faced box function
   function createFacedBox(scene: Scene, px: number, py: number, pz: number, sx: number, sy: number, sz: number) {
     const mat = new StandardMaterial("mat");
     const texture = new Texture("/textures/lava.png");
@@ -67,44 +59,35 @@ import {
     box.position = new Vector3(px, py, pz);
 
     scene.registerAfterRender(function () {
-        box.rotate(new Vector3(2, 6, 4)/*axis*/, 0.02/*angle*/, Space.LOCAL);
+        box.rotate(new Vector3(10, 7, 5)/*axis*/, 0.4/*angle*/, Space.LOCAL);
     });
     return box;
   }
 
-  function createTorus(scene: Scene, px: number, py: number, pz: number) {
-    const torus = MeshBuilder.CreateTorus("torus", {});
-    torus.position = new Vector3(px, py, pz);
-    scene.registerAfterRender(function () {
-      torus.rotate(new Vector3(4, 8, 2)/*axis*/, 0.02/*angle*/, Space.LOCAL);
-    });
-    return torus;
-  }
 
 
   function createAnyLight(scene: Scene, index: number, px: number, py: number, pz: number, colX: number, colY: number, colZ: number, mesh: Mesh) {
-    // only spotlight, point and directional can cast shadows in BabylonJS
     switch (index) {
-      case 1: //hemispheric light
+      case 1: 
         const hemiLight = new HemisphericLight("hemiLight", new Vector3(px, py, pz), scene);
         hemiLight.intensity = 0.1;
         return hemiLight;
         break;
-      case 2: //spot light
+      case 2:
         const spotLight = new SpotLight("spotLight", new Vector3(px, py, pz), new Vector3(0, -1, 0), Math.PI / 3, 10, scene);
-        spotLight.diffuse = new Color3(colX, colY, colZ); //0.39, 0.44, 0.91
+        spotLight.diffuse = new Color3(colX, colY, colZ); 
         let shadowGenerator = new ShadowGenerator(1024, spotLight);
         shadowGenerator.addShadowCaster(mesh);
         shadowGenerator.useExponentialShadowMap = true;
         return spotLight;
         break;
-      case 3: //point light
-        const pointLight = new PointLight("pointLight", new Vector3(px, py, pz), scene);
-        pointLight.diffuse = new Color3(colX, colY, colZ); //0.39, 0.44, 0.91
-        shadowGenerator = new ShadowGenerator(1024, pointLight);
+      case 3:
+        const directionalLight = new DirectionalLight("directionalLight", new Vector3(px, py, pz), scene);
+        directionalLight.diffuse = new Color3(colX, colY, colZ);
+        shadowGenerator = new ShadowGenerator(1024, directionalLight);
         shadowGenerator.addShadowCaster(mesh);
         shadowGenerator.useExponentialShadowMap = true;
-        return pointLight;
+        return directionalLight;
         break;
     }
   }
@@ -114,37 +97,13 @@ import {
     light.intensity = 0.3;
     return light;
   }
+  function createDirectionalLight(scene: Scene) {
+    const light = new DirectionalLight("light", new Vector3(1, 0, 0), scene);
+    light.diffuse = new Color3(1, 0, 0);
+    light.intensity = 1;
+    return light;
+  }
 
-  //PREVIOUS METHODS
-  // function createSpotLight(scene: Scene, px: number, py: number, pz: number) {
-  //   var light = new SpotLight("spotLight", new Vector3(-1, 1, -1), new Vector3(0, -1, 0), Math.PI / 2, 10, scene);
-  //   light.diffuse = new Color3(0.39, 0.44, 0.91);
-	//   light.specular = new Color3(0.22, 0.31, 0.79);
-  //   return light;
-  // }
-  
-  function createSphere(scene: Scene) {
-    let sphere = MeshBuilder.CreateSphere(
-      "sphere",
-      { diameter: 2, segments: 32 },
-      scene,
-    );
-    sphere.position.y = 1;
-    return sphere;
-  }
-  
-  function createGround(scene: Scene, w: number, h: number, px: number) {
-    let ground = MeshBuilder.CreateGround(
-      "ground",
-      { width: w, height: h },
-      scene,
-    );
-    const Ground = new StandardMaterial("Ground");
-    Ground.diffuseTexture = new Texture("/textures/lava.png");
-    ground.receiveShadows = true;
-    ground.position.x = px;
-    return ground;
-  }
   
   function createArcRotateCamera(scene: Scene) {
     let camAlpha = -Math.PI / 2,
@@ -162,10 +121,7 @@ import {
     camera.attachControl(true);
     return camera;
   }
-  //----------------------------------------------------------
-  
-  //----------------------------------------------------------
-  //BOTTOM OF CODE - MAIN RENDERING AREA FOR YOUR SCENE
+
   export default function createStartScene(engine: Engine) {
     interface SceneData {
       scene: Scene;
@@ -173,6 +129,7 @@ import {
       faceBox?: Mesh;
       light?: Light;
       hemisphericLight?: HemisphericLight;
+      directionalLight?: DirectionalLight;
       sphere?: Mesh;
       torus?: Mesh;
       ground?: Mesh;
@@ -182,32 +139,19 @@ import {
     let that: SceneData = { scene: new Scene(engine) };
     that.scene.debugLayer.show();
   
-    //createBox(scene, posX, posY, posZ, scalX, scalY, scalZ)
-    //Scene Lighting
+
     that.hemisphericLight = createHemiLight(that.scene);
-    //shape 1 and light
-    that.faceBox = createFacedBox(that.scene, -20, 2.5, 0, 0.5, 150, 150);
+    that.directionalLight = createDirectionalLight(that.scene);
+    that.faceBox = createFacedBox(that.scene, -10, 2.5, 0, 0.5, 1, 1);
     that.light = createAnyLight(that.scene, 2, -10, 5, 0, 0.75, 0.12, 0.91, that.faceBox);
 
-    //shape 2 and light
-    that.box = createBox(that.scene, -5, 2, 0, 3, 2, 1, true);
+    that.box = createBox(that.scene, -13, 2.5, 0, 0.5, 5, 9, false);
+    that.box = createBox(that.scene, -8, 2.5, 0, 0.5, 9, 9, false);
+    that.box = createBox(that.scene, -3, 2.5, 0, 0.5, 13, 9, false);
+    that.box = createBox(that.scene, 2, 2.5, 0, 0.5, 16, 9, false);
+    that.box = createBox(that.scene, 7, 2.5, 0, 0.5, 20, 9, false);
+    that.box = createBox(that.scene, 12, 2.5, 0, 0.5, 24, 9, false); 
 
-    //shape 3 and light
-    that.torus = createTorus(that.scene, 0, 2, 0);
-    that.light = createAnyLight(that.scene, 2, 0, 5, 0, 0.12, 0.64, 0.86, that.torus);
-
-
-    //walls
-    that.box = createBox(that.scene, -13, 2.5, 0, 0.5, 5, 9, false); //wall 
-    that.box = createBox(that.scene, -8, 2.5, 0, 0.5, 5, 9, false); //wall
-    that.box = createBox(that.scene, -3, 2.5, 0, 0.5, 5, 9, false); //wall 
-    that.box = createBox(that.scene, 2, 2.5, 0, 0.5, 20, 9, false); //wall 
-    that.box = createBox(that.scene, 7, 2.5, 0, 0.5, 5, 9, false); //wall 
-    that.box = createBox(that.scene, 12, 2.5, 0, 0.5, 5, 9, false); //wall 
-
-    //ground and main camera
-    that.ground = createGround(that.scene, 25, 10, -0.5);
     that.camera = createArcRotateCamera(that.scene);
     return that;
   }
-  //----------------------------------------------------
